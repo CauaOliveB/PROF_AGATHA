46eecafc68a8942237c55d058ee7c28dd31ac082
Cauã Olive Barbosa <cauaolivebarbosa@gmail.com>
2025-02-03 10:43:17 -0300

    Ex1

---
+++ src/EX1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EX1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Digite a temperatura(F)° :");;
+        int Fahrenheit = scanner.nextInt();
+
+        System.out.println("Fahrenheit:" + Fahrenheit + "F°");
+
+        double CelsiusDecimal = (5.0 / 9.0) * (Fahrenheit - 32);
+        int Celsius = (int) Math.round(CelsiusDecimal);
+
+        System.out.println("Celcius:" + Celsius + "C°");
+
+        scanner.close();
+
+    }
+    }
