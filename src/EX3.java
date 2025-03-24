75bafb22293b5a500d7f6c2c197d067472eb83d0
Cauã Olive Barbosa <cauaolivebarbosa@gmail.com>
2025-03-24 11:30:46 -0300

    TryCatch-Calculator

--- src/EX3.java
+++ src/EX3.java
@@ -3,5 +3,31 @@ public class EX3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Insira seu peso:");
+        int peso = scan.nextInt();
+
+        System.out.println("Insira sua altura:");
+        double altura = scan.nextDouble();
+
+        double imc = peso/ (altura*altura);
+
+        System.out.printf("Seu IMC é: %.2f%n", imc);
+
+        if (imc <= 18.5) {
+            System.out.println("Sua categoria é Magreza");
+        } else if (imc <= 24.9) {
+            System.out.println("Sua categoria é Normal");
+        } else if (imc <= 29.9) {
+            System.out.println("Sua categoria é Sobrepeso");
+        } else if (imc <= 34.9) {
+            System.out.println("Sua categoria é Obesidade Grau I");
+        } else if (imc <= 39.9) {
+            System.out.println("Sua categoria é Obesidade Grau II");
+        } else {
+            System.out.println("Sua categoria é Obesidade Grau III");
+        }
+
+        scan.close();
+
     }
 }
--- src/Treino.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class Treino {
-    public static void main(String[] args) {
-        Scanner scan =  new Scanner(System.in);;
-        System.out.printf("");
-    }
-}
